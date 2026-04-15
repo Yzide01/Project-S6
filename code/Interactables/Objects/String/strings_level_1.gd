2248f8dce2a20b066fd2b7fed86e83e36d7bc6e2
Zide Yang <zideyangcode@gmail.com>
2026-04-15 19:02:26 +0200

    add string sound

--- code/Interactables/Objects/String/strings_level_1.gd
+++ code/Interactables/Objects/String/strings_level_1.gd
@@ -2,16 +2,23 @@ extends Node2D
 
 signal played(id: int)
 
-@onready var interactable_area = $Interactable
+@onready var interactable = $Interactable
 
 func _ready() -> void:
-	if interactable_area:
-		interactable_area.interact_name = "Pluck the string"
-		interactable_area.is_interactable = true
+	if interactable:
+		interactable.interact_name = "Jouer la corde"
+		interactable.is_interactable = true
 		# On lie l'action à ce script
-		interactable_area.interact = _on_interact
+		interactable.interact = _on_interact
 
 func _on_interact():
-	var id = int(name.get_slice("_", 1))
+	var id_str = name.get_slice("_", 1)
+	var id = int(id_str)
+	
+	var sound_node_name = "Pluck_" + id_str
+	
+	if has_node(sound_node_name):
+		get_node(sound_node_name).play()
+
 	played.emit(id)
 	print("Corde jouée : ", id)
