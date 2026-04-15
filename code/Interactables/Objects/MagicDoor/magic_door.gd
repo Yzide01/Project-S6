b314978f5d9140925c276a04786b864556fb6464
Zide Yang <zideyangcode@gmail.com>
2026-04-15 18:36:36 +0200

    level 1 progress

--- code/Interactables/Objects/MagicDoor/magic_door.gd
+++ code/Interactables/Objects/MagicDoor/magic_door.gd
@@ -1,18 +1,25 @@
 extends Node2D
 
+signal hint_requested # Signal envoyé au niveau pour jouer le son
+
 @onready var interactable_area = $Interactable
+var is_locked: bool = true
 
 func _ready() -> void:
 	if interactable_area:
-		interactable_area.interact_name = "Mélodie requise"
-		interactable_area.is_interactable = false # Bloqué au début
+		interactable_area.interact_name = "Écouter l'indice"
+		interactable_area.is_interactable = true 
 		interactable_area.interact = _on_interact
 
 func _on_interact():
-	print("Le joueur traverse la porte !")
+	if is_locked:
+		print("Indice demandé à la porte !")
+		hint_requested.emit()
+	else:
+		print("Le joueur traverse la porte !")
+		# get_tree().change_scene_to_file("res://Levels/niveau2...") POUR PLUS TARD
 
-# Fonction appelée par le gestionnaire du niveau
 func unlock() -> void:
+	is_locked = false
 	if interactable_area:
-		interactable_area.is_interactable = true
 		interactable_area.interact_name = "Passer la porte"
--- code/Levels/niveau_1_corde_v_2.gd
+++ code/Levels/niveau_1_corde_v_2.gd
@@ -2,39 +2,64 @@ extends Node2D
 
 @export var secret_combination: Array[int] = [1, 3, 2]
 var current_sequence: Array[int] = []
+var is_playing_hint: bool = false # Empêche le spam pendant le son
 
 @onready var corde1 = $String_1
 @onready var corde2 = $String_2
 @onready var corde3 = $String_3
 @onready var magic_door = $MagicDoor 
+@onready var audio_hint = $HintAudioPlayer 
 
 func _ready() -> void:
-	# On connecte les signaux émis par tes Node2D
 	if corde1: corde1.played.connect(_on_string_played)
 	if corde2: corde2.played.connect(_on_string_played)
 	if corde3: corde3.played.connect(_on_string_played)
+	
+	if magic_door:
+		magic_door.hint_requested.connect(_on_door_hint_requested)
+
+func _on_door_hint_requested() -> void:
+	if is_playing_hint:
+		return
+		
+	is_playing_hint = true
+	current_sequence.clear() # On remet à zéro quand on redemande l'indice
+	print("Lecture de l'indice sonore...")
+	
+	if audio_hint:
+		audio_hint.play()
+		await audio_hint.finished 
+	else:
+		await get_tree().create_timer(3.0).timeout 
+		
+	is_playing_hint = false
+	print("Fin de l'indice, à vous de jouer.")
 
 func _on_string_played(id: int) -> void:
+	if is_playing_hint:
+		return
+
+	# On ajoute la note jouée
 	current_sequence.append(id)
+	
+	if current_sequence.size() > secret_combination.size():
+		current_sequence.pop_front()
+
 	print("Séquence en cours : ", current_sequence)
 
-	if current_sequence.size() == secret_combination.size():
-		if current_sequence == secret_combination:
-			_solve_puzzle()
-		else:
-			print("Mauvaise mélodie ! On efface tout.")
-			current_sequence.clear()
+	# On vérifie simplement si les 3 dernières notes sont les bonnes
+	if current_sequence == secret_combination:
+		_solve_puzzle()
 
 func _solve_puzzle() -> void:
-	print("Énigme résolue ! La porte s'ouvre.")
-	
+	print("Énigme résolue ! La porte est déverrouillée.")
 	if magic_door:
 		magic_door.unlock()
 	
-	# On désactive l'interaction des cordes pour bloquer le jeu
-	if corde1 and corde1.has_node("InteractableArea"):
-		corde1.get_node("InteractableArea").is_interactable = false
-	if corde2 and corde2.has_node("InteractableArea"):
-		corde2.get_node("InteractableArea").is_interactable = false
-	if corde3 and corde3.has_node("InteractableArea"):
-		corde3.get_node("InteractableArea").is_interactable = false
+	# Mise à jour avec le bon nom "Interactable"
+	if corde1 and corde1.has_node("Interactable"):
+		corde1.get_node("Interactable").is_interactable = false
+	if corde2 and corde2.has_node("Interactable"):
+		corde2.get_node("Interactable").is_interactable = false
+	if corde3 and corde3.has_node("Interactable"):
+		corde3.get_node("Interactable").is_interactable = false
