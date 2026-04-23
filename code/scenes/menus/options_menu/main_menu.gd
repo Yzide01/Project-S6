e226a92ee123b9bb0a61b74a9768da4a8387f2a6
Zide Yang <zideyangcode@gmail.com>
2026-04-23 21:41:40 +0200

    link scenes together

--- code/scenes/menus/options_menu/main_menu.gd
+++ code/scenes/menus/options_menu/main_menu.gd
@@ -12,7 +12,7 @@ func _on_play_pressed():
 	# On lance la Cinématique 1 ! 
 	# Le SceneManager s'occupe du fondu et de la vidéo.
 	# Après la vidéo, le SceneManager ira vers la scène de la Cinématique 2.
-	SceneManager.jouer_cinematique("res://Assets/Videos/Scene-1.ogv", "res://scenes/manager/cine2.tscn")
+	SceneManager.jouer_cinematique("res://Assets/Videos/Scène-1.ogv", "res://scenes/manager/relais_cine2.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit() # Ferme le jeu
