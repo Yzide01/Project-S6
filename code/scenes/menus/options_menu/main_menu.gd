8af6092ffbfa02b59d64c1fcf4fd8dfb2c91dd6f
Yzide01 <zideyangcode@gmail.com>
2026-04-25 15:16:24 +0200

    disable auto translation

--- code/scenes/menus/options_menu/main_menu.gd
+++ code/scenes/menus/options_menu/main_menu.gd
@@ -1,20 +1,16 @@
 extends Control
 
 func _ready():
-	# On connecte les signaux des boutons
 	$VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
 	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
 	
-	# Optionnel : S'assurer que la souris est visible
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_play_pressed():
 	hide()
-	# On lance la Cinématique 1 ! 
-	# Le SceneManager s'occupe du fondu et de la vidéo.
-	# Après la vidéo, le SceneManager ira vers la scène de la Cinématique 2.
+	# Play the cinematics
 	SceneManager.jouer_cinematique("res://Assets/Videos/Scène-1.ogv", "res://scenes/manager/relais_cine2.tscn", "res://Assets/Sound/Ambiant sound/cinematics.ogg")
 
 	
 func _on_quit_pressed():
-	get_tree().quit() # Ferme le jeu
+	get_tree().quit()
