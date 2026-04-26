45d414dae49af87e153612bad8e48a19c39fa5a6
Zide Yang <zideyangcode@gmail.com>
2026-04-26 17:28:09 +0200

    fix hotbar size + level 2 add to inventory

--- code/Interactables/Objects/Level2/sheets.gd
+++ code/Interactables/Objects/Level2/sheets.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @onready var interactable_area = $Interactable
 @onready var sprite = $Sprite2D
+# Load the inventory and the item
+@onready var inventory: Inventory = preload("res://Core/InventorySystem/playerInventory.tres")
+@onready var partition_item: InventoryItem = preload("res://Core/InventorySystem/items/Partition.tres")
+
 
 func _ready() -> void:
 	if interactable_area:
@@ -17,5 +21,8 @@ func _on_interact():
 		piano.ajouter_partition()
 		# Le dialogue spécifique au fragment (1, 2 ou 3) 
 		# sera géré par le piano pour être plus centralisé.
+	inventory.insert(partition_item)
+	
+	# 2. Your existing code to make it disappear (e.g., queue_free(), or erasing the tile)
 	
 	queue_free()
