a1aaa8823fda48ce884fc6d21d9ebf318d8388df
Yzide01 <zideyangcode@gmail.com>
2026-05-10 12:05:59 +0200

    open door sprite level 1

--- code/Interactables/Objects/MagicDoor/magic_door.gd
+++ code/Interactables/Objects/MagicDoor/magic_door.gd
@@ -3,9 +3,13 @@ extends Node2D
 signal hint_requested # Signal envoyé au niveau pour jouer le son
 
 @onready var interactable_area = $Interactable
+@onready var open_sprite = $Sprite2D
+@onready var behind_door = $Sprite2D2
 var is_locked: bool = true
 
 func _ready() -> void:
+	behind_door.visible = false
+	open_sprite.visible = false
 	if interactable_area:
 		interactable_area.interact_name = "Magic door"
 		interactable_area.is_interactable = true 
@@ -20,6 +24,8 @@ func _on_interact():
 		SceneManager.changer_niveau("res://Levels/niveau2_corde.tscn")
 
 func unlock() -> void:
+	open_sprite.visible = true
+	behind_door.visible = true
 	is_locked = false
 	if interactable_area:
 		interactable_area.interact_name = "Go through the door"
