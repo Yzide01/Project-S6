07182e73238a85fb6330e947d5625911265874f1
Zide <zideyangcode@gmail.com>
2026-05-20 17:03:15 +0200

    small fixes turn based

--- code/Core/CombatSystem/CombatManager.gd
+++ code/Core/CombatSystem/CombatManager.gd
@@ -117,17 +117,17 @@ func _check_test_mode(level: int = 1) -> void:
 		# --- LEVEL 1 : The Whisper alone ---
 		if level >= 1:
 			intro_message = "You only have your Strings, this attack doesn't deal much damage, but it lets you thin out the crowd—and who knows, maybe it'll scare them off\nThe Whisper is a fragile minion, but its silence is deadly."
-			horde.append(_create_enemy_data("Whisper", 25, 1))
+			horde.append(_create_enemy_data("Whisper", 20, 1))
 
 		# --- LEVEL 2 : We ADD the Silencer ---
 		if level >= 2:
 			intro_message = "You unlocked Percussions! Use Thunder Strike to shatter shields or deal heavy damage.\nWatch out for the Dampener. It looks sturdy and soundproof; I probably wouldn't do much damage to it, especially not while it has its shield up. But it looks slow to me, so I shouldn't take too much damage."
-			horde.append(_create_enemy_data("Silencer", 60, 2))
+			horde.append(_create_enemy_data("Dampener", 40, 2))
 
 		# --- LEVEL 3 : We ADD the Devourer ---
 		if level >= 3:
 			intro_message = "The final trial! You must face all three types of enemies at once. Use your Strings, Percussions, and your newly unlocked Winds to secure victory!"
-			horde.append(_create_enemy_data("Devourer", 150, 3))
+			horde.append(_create_enemy_data("Devourer", 25, 3))
 
 		# We launch the combat
 
@@ -182,7 +182,7 @@ func start_encounter(horde: Array[BaseEnemy], skills: Array[String], intro_text:
 			"hp": enemy_data.max_hp,
 			"max_hp": enemy_data.max_hp,
 			"stunned": false,
-			"has_shield": enemy_data.rank >= 2,
+			"has_shield": enemy_data.rank == 2,
 			"rank": enemy_data.rank,
 			"ui_node": enemy_ui
 		})
